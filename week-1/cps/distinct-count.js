6f935cdb0ce06a9d442936f345358880b5964946
mostofareza3 <mostofa.reza3@gmail.com>
2025-06-04 07:45:47 +0600

    distinct count problem solved

--- week-1/cps/distinct-count.js
+++ week-1/cps/distinct-count.js
@@ -31,3 +31,14 @@ Average
 Good
 
 */
+
+function distictArray(arr){
+    let uniqueArray = []
+    for(let i=0; i<arr.length; i++){
+        if(!uniqueArray.includes(arr[i])){
+            uniqueArray.push(arr[i])
+        }
+    }
+
+    return uniqueArray
+}
